5ca76d90c10cd3950c0ac570ad79b74069b4b8b0
kempsd <kemps.dorian@gmail.com>
2015-01-16 14:00:48 +0100

    source du premier jet du code
    Ajout des classes Catégorie et Image ainsi que leurs facades, plus la
    facade abstraite

---
+++ src/ejb/FacadeCategorie.java
@@ -0,0 +1,23 @@
+package ejb;
+
+import javax.ejb.Stateless;
+import javax.persistence.*;
+
+import model.Categorie;
+
+/**
+ * EJB session pour la classe Categorie
+ */
+@Stateless
+public class FacadeCategorie extends FacadeAbstraite {
+	@PersistenceContext(unitName = "JEE")
+	private EntityManager em;
+
+	protected EntityManager getEntityManager() {
+		return em;
+	}
+
+	public FacadeCategorie() {
+		super(Categorie.class);
+	}
+}
